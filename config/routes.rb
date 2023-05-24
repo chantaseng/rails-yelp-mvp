ef2ea3eaccc6bbeeb43b843c2d863e69f39a2207
William Chan <chan.william.ts@gmail.com>
2023-05-24 15:40:12 -0400

    coding in silo

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,21 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :restaurants, only: [:index, :new, :create, :show, :edit, :update, :destroy]
 
   # Read all
-  get '/restaurants', to: 'restaurants#index'
+  # get '/restaurants', to: 'restaurants#index'
 
   # Create
-  get '/restaurants/new', to: 'restaurants#new'
-  post '/restaurants', to: 'restaurants#create'
+  # get '/restaurants/new', to: 'restaurants#new' as: new_restaurant
+  # post '/restaurants', to: 'restaurants#create'
 
   # Read one
-  get '/restaurants/:id', to: 'restaurants#show'
+  # get '/restaurants/:id', to: 'restaurants#show' as: restaurant
 
   # Update
-  get '/restaurants/:id/edit', to: 'restaurants#edit'
-  patch '/restaurants/:id', to: 'restaurants#update'
+  # get '/restaurants/:id/edit', to: 'restaurants#edit'
+  # patch '/restaurants/:id', to: 'restaurants#update'
 
   # Delete
-  delete '/restaurants/:id', to: 'restaurants#destroy'
+  # delete '/restaurants/:id', to: 'restaurants#destroy'
 
 end
